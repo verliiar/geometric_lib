65b44f168f6b35d4a85a6de389a37588a8d58168
verliiar <jverliar@gmail.com>
2025-02-21 14:55:53 +0300

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,7 +1,15 @@
 
 def area_rectangle(a, b):
-    return a * b
+    if a >= 0 and b >= 0:
+        return a * b
+    return -1
 
 
 def perimeter_rectangle(a, b, c, d):
-    return a + b + c + d
+    if a != 0 and b != 0 and c != 0 and d != 0:
+        if (a == b and c == d) or (a == c and b == d) or (a == d and b == c):
+            return a + b + c + d
+        else:
+            return -1
+    else:
+        return 0
